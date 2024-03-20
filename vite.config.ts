39546c9435376b069bdc587dd048e02bb42c1245
PardeepDuple <pradeep.duple@gmail.com>
2024-03-20 18:28:23 +0530

    removed extra code

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/admin/"
+  base: "/admin/",
+  build: {
+    // Enable dynamic imports
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
