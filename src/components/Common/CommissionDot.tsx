0975328d03df6629a872c67a84a025d47b8e00ec
PardeepDuple <pradeep.duple@gmail.com>
2024-12-16 17:01:05 +0530

    commission dot in session position

--- src/components/Common/CommissionDot.tsx
+++ src/components/Common/CommissionDot.tsx
@@ -4,8 +4,8 @@ const CommissionDot = () => {
   return (
     <Box
       sx={{
-        width: 10,
-        height: 10,
+        width: "10px",
+        height: "10px",
         borderRadius: "50%",
         backgroundColor: "#74ee15",
         marginRight: "5px",
