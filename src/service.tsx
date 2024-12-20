338b7b4875abe20355a92ed79aa92ff84533a0e4
PardeepDuple <pradeep.duple@gmail.com>
2024-12-20 17:42:04 +0530

    fixed previous data getting removed when multiple request hit in single api

--- src/service.tsx
+++ src/service.tsx
@@ -97,7 +97,7 @@ service.interceptors.response.use(
   (error) => {
     if (axios.isCancel(error)) {
       console.log("Request canceled:", error.message);
-      return;
+      // return;
     }
 
     let { status, data } = error.response || {};
