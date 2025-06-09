50229c71ee14a7e19838330ef31826321ed521cd
PardeepFairtechh <pardeep.kumar@fairtechsol.com>
2025-06-09 17:33:06 +0530

    Refactor session component click handlers for improved readability and maintainability; streamline styles in AllRateSeperate and SessionBetSeperate components; optimize RowHeaderDomain click logic; clean up unnecessary code in SessionComponentMatches.

--- src/components/matchDetail/CricketCasinoMarket/PlaceBetComponentWeb.tsx
+++ src/components/matchDetail/CricketCasinoMarket/PlaceBetComponentWeb.tsx
@@ -15,85 +15,83 @@ const PlaceBetComponentWeb = ({ newData, profitLoss, color, index }: any) => {
     (state: RootState) => state.match.matchList
   );
   return (
-    <>
-      <Box
-        onClick={() => {
-          if (marketAnalysis?.betType) {
-            const currBetPL = marketAnalysis?.betType?.session?.find(
-              (item: any) => item.betId === newData?.id
-            );
-            if (currBetPL) {
-              dispatch(
-                addRunAmount({
-                  id: newData?.id,
-                  name: newData?.name,
-                  type: !newData?.isManual
-                    ? "Session Market"
-                    : "Quick Session Market",
-                  matchId: newData?.matchId,
-                  proLoss: JSON.stringify(currBetPL?.profitLoss),
-                })
-              );
-            }
-          } else {
+    <Box
+      onClick={() => {
+        if (marketAnalysis?.betType) {
+          const currBetPL = marketAnalysis?.betType?.session?.find(
+            (item: any) => item.betId === newData?.id
+          );
+          if (currBetPL) {
             dispatch(
-              getSessionProLoss({
-                matchId: newData?.matchId,
+              addRunAmount({
                 id: newData?.id,
-                name: newData?.name ?? newData?.RunnerName,
+                name: newData?.name,
                 type: !newData?.isManual
                   ? "Session Market"
                   : "Quick Session Market",
+                matchId: newData?.matchId,
+                proLoss: JSON.stringify(currBetPL?.profitLoss),
               })
             );
           }
-        }}
+        } else {
+          dispatch(
+            getSessionProLoss({
+              matchId: newData?.matchId,
+              id: newData?.id,
+              name: newData?.name ?? newData?.RunnerName,
+              type: !newData?.isManual
+                ? "Session Market"
+                : "Quick Session Market",
+            })
+          );
+        }
+      }}
+      sx={{
+        background: "#0B4F26",
+        flexDirection: "row",
+        display: "flex",
+        alignItems: "center",
+        paddingX: ".2vw",
+        width: "10vw",
+        borderRadius: "5px",
+        height: "32px",
+        right: "11vw",
+        position: "absolute",
+      }}
+    >
+      <Box
         sx={{
-          background: "#0B4F26",
-          flexDirection: "row",
-          display: "flex",
+          width: "100%",
           alignItems: "center",
-          paddingX: ".2vw",
-          width: "10vw",
-          borderRadius: "5px",
-          height: "32px",
-          right: "11vw",
-          position: "absolute",
+          justifyContent: "center",
+          display: "flex",
         }}
       >
-        <Box
+        <Typography
           sx={{
-            width: "100%",
-            alignItems: "center",
-            justifyContent: "center",
-            display: "flex",
+            fontSize: { lg: profitLoss?.maxLoss ? ".65vw" : ".6vw" },
+            fontWeight: profitLoss?.maxLoss ? "bold" : "500",
+            color: "white",
           }}
         >
-          <Typography
-            sx={{
-              fontSize: { lg: profitLoss?.maxLoss ? ".65vw" : ".6vw" },
-              fontWeight: profitLoss?.maxLoss ? "bold" : "500",
-              color: "white",
-            }}
-          >
-            {!profitLoss?.profitLoss && !profitLoss?.betPlaced
-              ? "Profit/Loss"
-              : handleNumber(
-                  parseFloat(
-                    profitLoss?.betPlaced?.[index] ??
-                      profitLoss?.profitLoss?.[index]
-                  ).toFixed(2),
-                  color
-                )}
-          </Typography>
-          <img
-            src={UD}
-            style={{ width: "12px", height: "12px", marginLeft: "5px" }}
-            alt="icon"
-          />
-        </Box>
+          {!profitLoss?.profitLoss && !profitLoss?.betPlaced
+            ? "Profit/Loss"
+            : handleNumber(
+                parseFloat(
+                  profitLoss?.betPlaced?.[index] ??
+                    profitLoss?.profitLoss?.[index]
+                ).toFixed(2),
+                color
+              )}
+        </Typography>
+        <img
+          src={UD}
+          style={{ width: "12px", height: "12px", marginLeft: "5px" }}
+          alt="icon"
+        />
       </Box>
-    </>
+    </Box>
   );
 };
 
